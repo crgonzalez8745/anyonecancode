2e351ffd0440c0304c26e67359b53a5ad546333d
Cristina Gonzalez <123207203+crgonzalez8745@users.noreply.github.com>
2023-06-13 19:03:18 -0700

    Update name: Kattt in vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath: process.env.BASE_URL ? `/${process.env.BASE_URL}` : "/",
   pwa: {
-    name: "Milligram",
+    name: "Kattt",
     themeColor: "#4DBA87",
     msTileColor: "#000000",
     appleMobileWebAppCapable: "yes",
